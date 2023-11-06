4cc0b08715c9986432f4f804eafc32ef8623c9a3
mahbuburemon <mahbuburemon60@gmail.com>
2023-11-06 16:29:46 +0600

    update deleteimage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+       <BrowserRouter>  
+                
+                   
+                      <Routes>
+                      <Route path="/" element={<Home/>}/>
+                      <Route path="/home" element={<Home/>}/>
+                
+                    
+                      </Routes>
+            </BrowserRouter>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,55 +1,43 @@
-import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import './Home.css';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./Home.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 
 // Import your images here...
-import img1 from '../../images/img1.jpeg';
-import img2 from '../../images/img2.jpeg';
-import img3 from '../../images/img3.jpeg';
-import img4 from '../../images/img4.jpeg';
-import img5 from '../../images/img5.jpeg';
-import img6 from '../../images/img6.jpeg';
-import img7 from '../../images/img7.jpeg';
-import img8 from '../../images/img8.jpeg';
-import img9 from '../../images/img9.jpeg';
-import img10 from '../../images/img10.jpeg';
-import img11 from '../../images/img11.jpeg';
+import img1 from "../../images/img1.jpeg";
+import img2 from "../../images/img2.jpeg";
+import img3 from "../../images/img3.jpeg";
+import img4 from "../../images/img4.jpeg";
+import img5 from "../../images/img5.jpeg";
+import img6 from "../../images/img6.jpeg";
+import img7 from "../../images/img7.jpeg";
+import img8 from "../../images/img8.jpeg";
+import img9 from "../../images/img9.jpeg";
+import img10 from "../../images/img10.jpeg";
+import img11 from "../../images/img11.jpeg";
 
 const Home = () => {
-  // Define your images array with unique keys
   const images = [
-    { id: '1', src: img1 },
-    { id: '2', src: img2 },
-    { id: '3', src: img3 },
-    { id: '4', src: img4 },
-    { id: '5', src: img5 },
-    { id: '6', src: img6 },
-    { id: '7', src: img7 },
-    { id: '8', src: img8 },
-    { id: '9', src: img9 },
-    { id: '10', src: img10 },
-    { id: '11', src: img11 },
+    { id: "1", src: img1 },
+    { id: "2", src: img2 },
+    { id: "3", src: img3 },
+    { id: "4", src: img4 },
+    { id: "5", src: img5 },
+    { id: "6", src: img6 },
+    { id: "7", src: img7 },
+    { id: "8", src: img8 },
+    { id: "9", src: img9 },
+    { id: "10", src: img10 },
+    { id: "11", src: img11 },
   ];
-  // const images = [
-  //   { id: '1', src: img1 , name: 'Item 1' },
-  //   { id: '2', src: img2 , name: 'Item 2'},
-  //   { id: '3', src: img3 , name: 'Item 3'},
-  //   { id: '4', src: img4 , name: 'Item 4'},
-  //   { id: '5', src: img5 , name: 'Item 5'},
-  //   { id: '6', src: img6 , name: 'Item 6'},
-  //   { id: '7', src: img7 , name: 'Item 7'},
-  //   { id: '8', src: img8 , name: 'Item 8'},
-  //   { id: '9', src: img9 , name: 'Item 9'},
-  //   { id: '10', src: img10 , name: 'Item 10'},
-  //   { id: '11', src: img11 , name: 'Item 11'},
-  // ];
 
   const [imageList, setImageList] = useState(images);
-  const [seclete, setSeclete] =useState(false)
-  const [count, setCount] = useState(0);
-  const [selectedItems, setSelectedItems] = useState([]);
-
-  
+  const [selectedImages, setSelectedImages] = useState([]);
 
   const onDragEnd = (result) => {
     if (!result.destination) {
@@ -63,36 +51,53 @@ const Home = () => {
     setImageList(items);
   };
 
-  const toggleSeclete = e =>{
-    setSeclete(e.target.checked)
-  }
-
-  const hendleClick= e=>{
-    setCount(count + 1);
-  }
-
- 
+  const toggleSelect = (imageId) => {
+    if (selectedImages.includes(imageId)) {
+      setSelectedImages((prevSelectedImages) =>
+        prevSelectedImages.filter((id) => id !== imageId)
+      );
+    } else {
+      setSelectedImages([...selectedImages, imageId]);
+    }
+  };
+// deleteSelectedImages 
+  const deleteSelectedImages = () => {
+    const updatedImageList = imageList.filter(
+      (image) => !selectedImages.includes(image.id)
+    );
+    setImageList(updatedImageList);
+    setSelectedImages([]);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div className="gridd">
-        <div className="line">
-          
-         {seclete &&  <div className="row">
-                          <div className="col-md-6">
-                            <p className="text-start p-2">Seleted Item:{count}</p>
-                          </div>
-                          <div className="col-md-6 ">
-                          <p className='text-end p-2' ><a href="#" 
-                           className="link-danger ">Delete File</a></p>
-                            </div>                
-                       </div> 
-         ||
-         <p className="text-start p-2"> Gallery </p>
-         }
-          
-          <hr />
-        </div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="line">
+              {selectedImages.length > 0 ? (
+                <Row>
+                  <Col md={6}>
+                    <p className="text-start p-2">
+                      Selected Item: {selectedImages.length}
+                    </p>
+                  </Col>
+                  <Col md={6}>
+                    <p className="text-end p-2">
+                      <Button variant="danger" onClick={deleteSelectedImages}>
+                        Delete File
+                      </Button>
+                    </p>
+                  </Col>
+                </Row>
+              ) : (
+                <p className="text-start p-2">Gallery</p>
+              )}
+              <hr />
+            </div>
+          </Col>
+        </Row>
+
         <Droppable droppableId="gallery" direction="horizontal">
           {(provided) => (
             <div
@@ -101,26 +106,47 @@ const Home = () => {
               className="row"
             >
               {imageList.map((image, index) => (
-                <Draggable key={image.id} draggableId={image.id} index={index}>
+                <Draggable
+                  key={image.id}
+                  draggableId={image.id}
+                  index={index}
+                >
                   {(provided) => (
-                    <div
-                    className={`col-md-${index % 12 === 0 ?5 : index % 2 === 6 ? 7 : index % 2 === 2 ? 4 : 4} p-2 `}
-                    // style={index % 12 === 0 ? { height: '295px' ,width:'100%' } : {}}
-                    
+                    <Col
+                      md={
+                        index % 12 === 0
+                          ? 4
+                          : index % 2 === 3
+                          ? 6
+                          : index % 2 === 5
+                          ? 12
+                          : 2
+                      }
+                      className="p-2 column"
                       ref={provided.innerRef}
                       {...provided.draggableProps}
                       {...provided.dragHandleProps}
+                      
                     >
-                      <div className="card">
-                        <img src={image.src} className="image" alt="..." />
-                        <div class="middle">
-                        <div class="form-check ">
-                            <input class="form-check-input checkbox" onChange={toggleSeclete} onClick={hendleClick} type="checkbox" value="" id="flexCheckDefault"/>
-                            
-                          </div>
+                      <Card>
+                        <Card.Img
+                          src={image.src}
+                          className="image"
+                          alt="..."
+                          onClick={() => toggleSelect(image.id)}
+                        />
+                        <div className="common_overlay">
+                          <Form.Check>
+                            <Form.Check.Input
+                              className="checkbox"
+                              type="checkbox"
+                              checked={selectedImages.includes(image.id)}
+                              onChange={() => toggleSelect(image.id)}
+                            />
+                          </Form.Check>
                         </div>
-                      </div>
-                    </div>
+                      </Card>
+                    </Col>
                   )}
                 </Draggable>
               ))}
@@ -128,7 +154,7 @@ const Home = () => {
             </div>
           )}
         </Droppable>
-      </div>
+      </Container>
     </DragDropContext>
   );
 };
